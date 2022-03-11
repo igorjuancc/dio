266406a97ffd0a2320de209d2fa03de139058c78
Igor Juan <igorjuancc@gmail.com>
2022-03-11 15:03:20 -0300

    inicio microsservico

---
+++ Botcamps/GFT_Start_4_Java/utilizando-spring/src/main/java/com/dio/personapi/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.dio.personapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.dio.personapi.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title("API Person")
+                .description("REST API Person para gerenciamento de pessoas.")
+                .version("1.0.0")
+                .contact(new springfox.documentation.service.Contact(
+                        "Igor Costa",
+                        "github.com/igorjuancc",
+                        null))
+                .build();
+    }
+
+}
